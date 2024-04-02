a944dac3ea9e69851c1eb936ffa8af24d7d7e8fe
Julien-epi <julienranson77@gmail.com>
2024-04-02 15:15:50 +0200

    connect wallet with raimbowkit and navbar

---
+++ authentywatch/src/components/Layout/Footer.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const Footer = () => {
+  return (
+    <footer className="fixed inset-x-0 bottom-0 text-center p-4">
+      <p>© 2024</p>
+    </footer>
+  );
+};
+
+export default Footer;

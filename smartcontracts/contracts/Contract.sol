c2e156d4dd5fea5dd34b228130ed4fbf6a5b8735
coralieBo <coralieboyer@Coralies-MacBook-Pro.local>
2024-04-03 10:42:18 +0200

    V2 contract + unitary tests

--- smartcontracts/contracts/Contract.sol
+++ smartcontracts/contracts/Contract.sol
@@ -6,31 +6,31 @@ pragma solidity >=0.8.2 <0.9.0;
 
 contract Authentywatch is ERC721{
     address[] internal admins;
-    uint256 public ids;
+    uint256 public ids = 1;
 
     mapping(uint256 => string) internal URIs;
 
 	event NewNFTMinted(address indexed from, uint256 itemId);
 
-    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) { 
-        admins[0] = msg.sender;
+    constructor(string memory name, string memory symbol) ERC721(name, symbol) {
+        admins.push(msg.sender);
     }
 
     modifier onlyAdmins(){
-        bool isAdmin = false;
+        bool admin = false;
         uint size = admins.length;
         for (uint i=0; i < size; i++){
             if(msg.sender == admins[i]){
-                isAdmin = true;
+                admin = true;
             }
         }
-        require(isAdmin, "Caller is not admin");
+        require(admin, "Caller is not admin");
         _;
     }
 
-    function mint(string memory _URI) external onlyAdmins{
+    function mint(address to, string memory _URI) external onlyAdmins{
         uint256 id = ids;
-        _mint(msg.sender, id); // TODO : VOIR S'IL FAUT CHANGER MSG.SENDER
+        _mint(to, id);
         URIs[id] = _URI;
         ids++;
     }
@@ -40,16 +40,18 @@ contract Authentywatch is ERC721{
     }
 
     function tokenURI(uint256 _tokenId) public view override returns(string memory){
+        require(_tokenId < ids, "ID out of range");
         return URIs[_tokenId];
     }
 
     function getTokens(uint256 _idStart, uint256 _idStop) external view returns(string[] memory){
         require(_idStop < ids, "ID out of range");
         require(_idStart > 0, "ID out of range");
-        require(_idStop - _idStart > 0, "ID out of range");
-        string[] memory tokens;
+        require(_idStop - _idStart >= 0, "ID out of range");
+        uint size = _idStop - _idStart + 1;
+        string[] memory tokens = new string[](size);
         uint index = 0;
-        for(uint i = _idStart; i < _idStop ; i++){
+        for(uint i = _idStart; i <= _idStop ; i++){
             tokens[index] = URIs[i];
         }
         return tokens;
@@ -59,16 +61,29 @@ contract Authentywatch is ERC721{
         return URIs[_tokenId];
     }
 
-    function getAdmins() external view returns(address[] memory){
-        return admins;
+    function isAdmin(address _account) external view returns(bool){
+        bool admin = false;
+        uint size = admins.length;
+        for (uint i=0; i < size; i++){
+            if(_account == admins[i]){
+                admin = true;
+            }
+        }
+        return admin;
     }
 
-    function setAdmin(address _newAdmin) external onlyAdmins{
+    function addAdmin(address _newAdmin) external onlyAdmins{
         admins.push(_newAdmin);
     }
 
     function removeAdmin(address _admin) external onlyAdmins{
-        // TODO
+        for (uint256 i = 0; i < admins.length; i++) {
+        if (admins[i] == _admin) {
+            admins[i] = admins[admins.length - 1];
+            admins.pop();
+            return;
+        }
+    }
     }
 
 }
--- smartcontracts/test/Contract.js
+++ smartcontracts/test/Contract.js
@@ -5,13 +5,11 @@ const {
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
 
-describe("CoupDePousse", function () {
+describe("Authentywatch", function () {
   let Contract;
   let contract;
-  let owner;
+  let admin;
   let user;
-  let association;
-  const decimals = 8;
 
   const errors = {
     NOT_ADMIN: "Caller is not admin",
@@ -21,7 +19,7 @@ describe("CoupDePousse", function () {
   before(async () => {
     [admin, user] = await ethers.getSigners();
     Contract = await ethers.getContractFactory("Authentywatch");
-    contract = await Contract.connect(owner).deploy("Authentywatch", "ATW");
+    contract = await Contract.connect(admin).deploy("Authentywatch", "ATW");
   });
 
   describe("initialization", function () {
@@ -36,21 +34,62 @@ describe("CoupDePousse", function () {
 
   describe("minting", function () {
     it("Should mint a token", async function () {
-      await contract.connect(owner).mint(user.address, 1);
+      await contract.connect(admin).mint(user.address, "");
       const balance = await contract.balanceOf(user.address);
 
       expect(balance).to.equal(1);
     });
 
-    it("Should mint multiple tokens", async function () {
-      await contract.connect(owner).mint(user.address, 2);
-      const balance = await contract.balanceOf(user.address);
-
-      expect(balance).to.equal(3);
-    });
-
     it("Should not mint a token if not admin", async function () {
-      await expect(contract.connect(user).mint(user.address, 1)).to.be.revertedWith(errors.NOT_ADMIN);
+      await expect(contract.connect(user).mint(user.address, "")).to.be.revertedWith(errors.NOT_ADMIN);
     });
+  });
+
+  describe("getting tokens URIs", function () {
+    it("Should get a token URI", async function () {
+      const uri = await contract.tokenURI(1);
+
+      expect(uri).to.equal("");
+    });
+
+    it("Should not get a token URI if out of range", async function () {
+      await expect(contract.tokenURI(2)).to.be.revertedWith(errors.OUT_OF_RANGE);
+    });
+
+    it("Should get all token URIs", async function () {
+      await contract.connect(admin).mint(user.address, "");
+      const uris = await contract.getTokens(1, 2);
+
+      expect(uris).to.have.lengthOf(2);
+    });
+
+    it("Should not get all token URIs if out of range", async function () {
+      await expect(contract.getTokens(2, 3)).to.be.revertedWith(errors.OUT_OF_RANGE);
+    });
+  });
+
+  describe("Managing admins", function () {
+    it("Should not add an admin if not admin", async function () {
+      await expect(contract.connect(user).addAdmin(user.address)).to.be.revertedWith(errors.NOT_ADMIN);
+    });
+
+    it("Should add an admin", async function () {
+      await contract.connect(admin).addAdmin(user.address);
+      const isAdmin = await contract.isAdmin(user.address);
+
+      expect(isAdmin).to.be.true;
+    });
+
+    it("Should remove an admin", async function () {
+      await contract.connect(admin).removeAdmin(user.address);
+      const isAdmin = await contract.isAdmin(user.address);
+
+      expect(isAdmin).to.be.false;
+    });
+
+    it("Should not remove an admin if not admin", async function () {
+      await expect(contract.connect(user).removeAdmin(user.address)).to.be.revertedWith(errors.NOT_ADMIN);
+    });
+
   });
 });

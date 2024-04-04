e6d23372985513611420c431d0672ac7f8d82bf3
lattana soucksengphet <lattana-s@hotmail.fr>
2024-04-04 20:51:51 +0200

    [APP] only admin can see admin links

--- authentywatch/src/components/Layout/Navbar.tsx
+++ authentywatch/src/components/Layout/Navbar.tsx
@@ -1,13 +1,11 @@
 import Link from "next/link";
 import React, { useState, useEffect, useRef } from "react";
 import ConnectWalletButton from "../ConnectWalletButton/ConnectWalletButton";
-// import { FaSun, FaMoon } from "react-icons/fa";
 import logo from "@/utils/assets/logo.png";
 import Image from "next/image";
-
+import { useAdminStatus } from "@/hooks/useAdminStatus";
 export default function Navbar() {
-  //   const { darkMode, setDarkMode } = useContext(AuthContext);
-
+  const isAdmin = useAdminStatus();
   return (
     <div className="fixed w-full flex justify-between items-center px-6 top-0 z-10 bg-black/20 backdrop-blur border-b border-neutral-600 py-1">
       <div className="flex items-center space-x-6 text-gray-200">
@@ -25,9 +23,11 @@ export default function Navbar() {
         <Link href="/scanNFC" className="hover:text-lightBlue duration-200">
           Scan
         </Link>
-        <Link href="/admin" className="hover:text-lightBlue duration-200">
-          Admin
-        </Link>
+        {isAdmin && (
+          <Link href="/admin" className="hover:text-lightBlue duration-200">
+            Admin
+          </Link>
+        )}
       </div>
 
       <ConnectWalletButton />
